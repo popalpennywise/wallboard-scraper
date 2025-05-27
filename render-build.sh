e786e2fe86e53aa80ec92b8596bec999f2c9d6be
popalpennywise <ot@infinetsolutions.co.uk>
2025-05-27 14:11:19 +0100

    render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies required for Puppeteer/Chromium
+apt-get update && apt-get install -y \
+  ca-certificates \
+  fonts-liberation \
+  libappindicator3-1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libgbm1 \
+  libgtk-3-0 \
+  libnspr4 \
+  libnss3 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils \
+  wget
+
+# Install Chromium
+npx puppeteer browsers install chrome
+
+# Install npm dependencies
+npm install
